d15deb31386a3819f6e2fdbb1bf1256b01f0d13e
CHEEKIBREEKIIVDAMKE <CHEEKIBREEKIIVDAMKE@DESKTOP-S13CU08>
2018-09-23 18:55:25 +0700

    rho-ink

---
+++ src/pbo3/pkg10117121/latihan8/pecahan/PBO310117121Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117121.latihan8.pecahan;
+
+/**
+ *
+ * @author CHEEKIBREEKIIVDAMKE
+ *NAMA                 : M Rifqi S M
+ *KELAS                : PBO-3 
+ *NIM                  : 10117121
+ *Program Deskripsi    : Program ini berisi program menampilkan pecahan
+ */
+public class PBO310117121Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789F;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
